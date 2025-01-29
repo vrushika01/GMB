60f1ee6b92fc0c56a80acbceedb8e83a3cf54b2a
vrushika patel <98327042+vrushika01@users.noreply.github.com>
2025-01-29 17:31:35 +0530

    Add files via upload

---
+++ portsData.js
@@ -0,0 +1,335 @@
+const portsData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Mandvi Port group",
+                "info": "Mandvi, a fair weather lighterage port , is situated on the right bank of the river Rukmavati in the Gulf of kutch on the west coast of India.",
+                "detailsPage":"mandvi.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.7211, 22.7766]
+            },
+            "ports": [
+                {
+                    "name": "Mundra (MPSEZ)",
+                    "coordinates": [69.721329, 22.839520],
+                    "detailsPage": "Mundra (MPSEZ).html"
+                },
+                {
+                    "name": "Jakhau",
+                    "coordinates": [68.6079, 23.2424],
+                    "detailsPage": "Jakhau.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Navlakhi Port group",
+                "info": "Navlakhi is a all weather lighterage port situated on the south east end of Gulf of Kutch, on the wester coast of India.",
+                "detailsPage":"navlakhi.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [70.4529,22.9574]
+            },
+            "ports": [
+                {
+                    "name": "GMB Jetty",
+                    "info": "GMB jetty info",
+                    "coordinates": [70.45, 22.966667],
+                    "detailsPage": "GMB Jetty.html"
+                }
+                ,
+                {
+                    "name": "Jaydeep",
+                    "coordinates": [ 70.44406 , 22.95847],
+                    "detailsPage": "Jaydeep.html"
+                },
+                // {
+                //     "name": "Chaugule",
+                //     "coordinates": [ , ],
+                //     "detailsPage": "Chaugule.html"
+                // },
+                {
+                    "name": "United Shippers ltd.",
+                    "coordinates": [ 70.08113 ,22.5465 ],
+                    "detailsPage": "United Shippers ltd.html"
+                },
+                {
+                    "name": "Shriji Shipping",
+                    "coordinates": [70.0757 ,22.48117 ],
+                    "detailsPage": "Shriji Shipping.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Porbandar Port group",
+                "info": "Porbandar is an all weather port with direct berthing facilities. It is situated on the west coast of Saurashtra, facing the Arabian sea.",
+                "detailsPage":"porbandar.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.5887, 21.6241]
+            },
+            "ports": [
+                {
+                    "name": "GMB Jetty(Porbandar)",
+                    "coordinates": [69.58883018391032,21.638318843953012],
+                    "detailsPage": "P.html"
+                },
+                {
+                    "name": "SAURASTRA Cement Co.",
+                    "coordinates": [69.60159659084132 ,21.650448009436545],
+                    "detailsPage": "SAURASTRA.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Veraval Port group",
+                "info": "Veraval, a fair weather lighterage port is located in the South west coast of Saurashtra on the west coast of India.",
+                "detailsPage":"veraval.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [70.3837, 20.9061]
+            },
+            "ports": [
+                {
+                    "name": "Mul-Dwarka",
+                    "coordinates": [ 70.65653 ,20.75702 ],
+                    "detailsPage": "Mul-Dwarka.html"
+                },
+                {
+                    "name": "Navabandar",
+                    "coordinates": [71.08, 20.74 ],
+                    "detailsPage": "Navabandar.html"
+                },
+                {
+                    "name": "Kotda",
+                    "coordinates": [70.8701419710241,20.723645198496428],
+                    "detailsPage": "kotda.html"
+                },
+                {
+                    "name": "Madhvad",
+                    "coordinates": [70.391505,20.894637],
+                    "detailsPage": "madhvad.html"
+                },
+                {
+                    "name": "Mangrol",
+                    "coordinates": [70.12, 21.12 ],
+                    "detailsPage": "mangrol.html"
+                },
+                {
+                    "name": "Chhara",
+                    "coordinates": [70.737544, 20.72168],
+                    "detailsPage": "chhara.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Jafrabad Port group",
+                "info": "Jafrabad is an all weather direct berthing port located inside Jafrabad creek in the Gulf of Cambay on the south coast of Saurashtra.",
+                "detailsPage":"jafrabad.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [71.3866, 20.8629]
+            },
+            "ports": [
+                {
+                    "name": "Ultratech Jetty(KOVAYA)",
+                    "coordinates": [ 71.3721668 , 20.8682842],
+                    "detailsPage": "ultratech.html"
+                },
+                {
+                    "name": "Gujrat Pipavav Port Ltd",
+                    "coordinates": [71.4965142, 20.922079],
+                    "detailsPage": "Gujrat Pipavav Port Ltd.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Bhavnagar Port group",
+                "info": "Bhavnagar is an all weather berthing port for smaller vessels located in the Gulf of Cambay on the west coast of India.",
+                "detailsPage":"bhavnagar.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.2308, 21.7669]
+            },
+            "ports": [
+                {
+                    "name": "Ghogha",
+                    "coordinates": [72.282922, 21.677171],
+                    "detailsPage": "Ghogha.html"
+                }
+                ,
+                {
+                    "name": "Talaja",
+                    "coordinates": [72.114063,21.315184],
+                    "detailsPage": "talaja.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Magdalla Port group",
+                "info": "Magdalla is a lighterage port located on the west coast of India in southern Gujarat. It is located on the bank of river Tapi.",
+                "detailsPage":"magdalla.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.7311, 21.14295]
+            },
+            "ports": [
+                {
+                    "name": "Magdalla port ",
+                    "coordinates": [72.75201746293834,21.156197409213593],
+                    "detailsPage": "Magdalla port.html"
+                },
+                {
+                    "name": "HAZIRA ",
+                    "coordinates": [72.6443, 21.0875],
+                    "detailsPage": "HAZIRA.html"
+                },
+                {
+                    "name": "Umargaon",
+                    "coordinates": [72.74751009962502,20.234150756067805],
+                    "detailsPage": "Umargaon.html"
+                },
+                {
+                    "name": "Maroli",
+                    "coordinates": [72.8899466, 21.0243434],
+                    "detailsPage": "Maroli.html"
+                },
+                {
+                    "name": "Valsad",
+                    "coordinates": [72.750, 20.233],
+                    "detailsPage": "valsad.html"
+                },
+                {
+                    "name": "Bilimora",
+                    "coordinates": [72.9539, 20.7669],
+                    "detailsPage": "Bilimora.html"
+                },
+                {
+                    "name": "Onjal ",
+                    "coordinates": [72.8355744402216,20.437948144660943],
+                    "detailsPage": "Onjal .html"
+                },
+                {
+                    "name": "Kolak",
+                    "coordinates": [72.883, 20.483],
+                    "detailsPage": "Kolak.html"
+                },
+                {
+                    "name": "Umarsadi",
+                    "coordinates": [72.83282354919598,20.455346283469833],
+                    "detailsPage": "Umarsadi.html"
+                },
+                {
+                    "name": "Vansi-Borsi",
+                    "coordinates": [ 72.7705,20.9289 ],
+                    "detailsPage": "Vansi-Borsi.html"
+                },
+                {
+                    "name": "Nargol",
+                    "coordinates": [72.78338076548859,20.243565815607447],
+                    "detailsPage": "Nargol.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Okha Port group",
+                "info": "Okha is an all weather port with direct berthing facilities located on the north-west coast of Saurashtra Peninsula.",
+                "detailsPage":"okha.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [69.0833, 22.4667]
+            },
+            "ports": [
+                {
+                    "name": "Bhogat",
+                    "coordinates": [69.24156, 21.9924159],
+                    "detailsPage": "bhogat.html"
+                },
+                {
+                    "name": "Rupen",
+                    "coordinates": [68.95417, 22.25944],
+                    "detailsPage": "rupen.html"
+                },
+                {
+                    "name": "Beyt",
+                    "coordinates": [69.0928497, 22.4521255],
+                    "detailsPage": "beyt.html"
+                }
+            ]
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Bharuch Port group",
+                "info": "Dahej is a fair weather lighterage port located at the junction of Guljaria and Ban Creek, on the west coast of the country, at Gulf of Cambay.",
+                "detailsPage":"Dahej.html"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [72.5843, 21.7132]
+            },"ports": [
+                {
+                    "name": "RDMT Jetty",
+                    "coordinates": [72.56089, 21.65976],
+                    "detailsPage": "RDMT Jetty.html"
+                },
+                {
+                    "name": "DHIL Jetty",
+                    "coordinates": [72.684505,21.684835],
+                    "detailsPage": "DHIL Jetty.html"
+                },
+                // {
+                //     "name": "Godrej Jetty",
+                //     "coordinates": [72.9404, 19.0973],
+                //     "detailsPage": "Godrej Jetty.html"
+                // },
+                {
+                    "name": "ISGECT Jetty",
+                    "coordinates": [72.591442,21.677741],
+                    "detailsPage": "ISGECT Jetty.html"
+                },
+                {
+                    "name": "GCPTCL(Dahej)",
+                    "coordinates": [72.53790, 21.69255],
+                    "detailsPage": "GCPTCL(Dahej).html"
+                },
+                {
+                    "name": "Petronet LNG (PLL)",
+                    "coordinates": [72.536, 21.6746],
+                    "detailsPage": "Petronet LNG (PLL).html"
+                }
+                ,
+                {
+                    "name": "Adani Petronet Pvt Ltd.",
+                    "coordinates": [72.53711747427097,21.689106167948733],
+                    "detailsPage": "Adani Petronet Pvt Ltd.html"
+                }
+                
+            ]
+        }
+    ]
+};
